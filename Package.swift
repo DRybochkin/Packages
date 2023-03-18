ce228e473eeef25c947244b7b2c0ab800ddd554b
DRybochkin <d-rybochkin@yandex.ru>
2023-03-19 02:33:58 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -1,1 +1,27 @@
+// swift-tools-version:5.7
 
+import PackageDescription
+
+let package = Package(
+    name: "TracerCrashReporter",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "TracerCrashReporter",
+            targets: ["TracerCrashReporter", "CrashReporter"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "TracerCrashReporter",
+            path: "Sources"
+        ),
+        .binaryTarget(
+            name: "CrashReporter",
+            url: "https://github.com/DRybochkin/TestXCRC/files/11009825/CrashReporter.xcframework.zip",
+            checksum: "520526b3f705139743dfcd33abcf2812e1071d4bedfe62c6921b9af58f5552a2")
+    ]
+)
