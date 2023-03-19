0213a252306137c556039d4c010deee583375fe9
DRybochkin <d-rybochkin@yandex.ru>
2023-03-19 18:34:55 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -3,25 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "TracerCrashReporter",
+    name: "OKTracerPackage",
     platforms: [
         .iOS(.v12),
     ],
     products: [
         .library(
-            name: "TracerCrashReporter",
-            targets: ["TracerCrashReporter", "CrashReporter"]),
+            name: "OKTracerPackage",
+            targets: ["OKTracerPackage", "OKTracer"]),
     ],
     dependencies: [
     ],
     targets: [
         .target(
-            name: "TracerCrashReporter",
+            name: "OKTracerPackage",
             path: "Sources"
         ),
         .binaryTarget(
-            name: "CrashReporter",
-            url: "https://github.com/DRybochkin/TestXCRC/files/11009825/CrashReporter.xcframework.zip",
-            checksum: "520526b3f705139743dfcd33abcf2812e1071d4bedfe62c6921b9af58f5552a2")
+            name: "OKTracer",
+            url: "https://github.com/DRybochkin/Packages/files/11011284/OKTracer.xcframework.zip",
+            checksum: "543dbca5f62a27194b4ef488bac6646b32617b3ca3ebbf4cb3ce0ee3561a8ba3")
     ]
 )
