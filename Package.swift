ba10f76e5e64919aad14402d5d6fe93eb992fc70
DRybochkin <d-rybochkin@yandex.ru>
2023-03-20 12:23:05 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -25,20 +25,6 @@ let package = Package(
         .binaryTarget(
             name: "OKTracer",
             url: "https://github.com/DRybochkin/Packages/files/11016003/OKTracer.xcframework.zip",
-            checksum: "31d2aff69c034f56bb76bbdf37a26e3eb5f058dbf74cc469a0acf06db14976a6",
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit"),
-                .linkedLibrary("z"),
-                .unsafeFlags(["-weak-lswiftDemangle"])
-            ],
-            cSettings: [
-                .define("PLCR_PRIVATE"),
-                .define("PLCF_RELEASE_BUILD"),
-                .define("PLCRASHREPORTER_PREFIX", to: ""),
-                .define("SWIFT_PACKAGE"),
-                .unsafeFlags(["-Wno-shorten-64-to-32"])
-            ]
-	)
+            checksum: "31d2aff69c034f56bb76bbdf37a26e3eb5f058dbf74cc469a0acf06db14976a6")
     ]
 )
