b7004d01b8362c9128a8e6bcd12ef369aed3c22b
DRybochkin <d-rybochkin@yandex.ru>
2023-04-05 14:30:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
        ),
         .binaryTarget(
             name: "OKTracer",
-            url: "https://github.com/DRybochkin/Packages/files/11016003/OKTracer.xcframework.zip",
+            url: "https://github.com/DRybochkin/Packages/files/11158158/OKTracer.xcframework.zip",
             checksum: "31d2aff69c034f56bb76bbdf37a26e3eb5f058dbf74cc469a0acf06db14976a6")
     ]
 )
