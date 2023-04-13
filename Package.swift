3def3e9593516be4cad1dc9bdd33fa17e8bb20f4
DRybochkin <d-rybochkin@yandex.ru>
2023-04-13 23:21:17 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OKTracer",
-            url: "https://nexus.odkl.ru/repository/dists/ios-debug-symbols/OKTracer/xcframeworks/0.1.19.31/OKTracerWrapperFramework.xcframework.zip",
-            checksum: "17977ace137c4410d6fdabe37cbf7dc51850fbad5ab28655266d3b9beffedf03")
+            url: "https://nexus.odkl.ru/repository/dists/ios-debug-symbols/OKTracer/xcframeworks/0.1.19.32/OKTracerWrapperFramework.xcframework.zip",
+            checksum: "13a9981099d60774960452f66e1a7890e3cc47b6749f797d983334a0f9e7b88e")
     ]
 )
